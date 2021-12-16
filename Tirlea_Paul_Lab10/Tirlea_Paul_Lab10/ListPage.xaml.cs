6da317cd41be844bc5fa41541f147aaa5abd5a71
Paul Sebastian Bogdan Tirlea <Tirlea.Al.Paul@student.utcluj.ro>
2021-12-16 15:16:48 +0200

    Am realizat laboratorul 11

--- Tirlea_Paul_Lab10/Tirlea_Paul_Lab10/ListPage.xaml.cs
+++ Tirlea_Paul_Lab10/Tirlea_Paul_Lab10/ListPage.xaml.cs
@@ -32,5 +32,24 @@ namespace Tirlea_Paul_Lab10
             await Navigation.PopAsync();
         }
 
+        async void OnChooseButtonClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new ProductPage((ShopList)
+           this.BindingContext)
+            {
+                BindingContext = new Product()
+            });
+
+        }
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+            var shopl = (ShopList)BindingContext;
+
+            listView.ItemsSource = await App.Database.GetListProductsAsync(shopl.ID);
+        }
+
+
     }
 }
---
+++ Tirlea_Paul_Lab10/Tirlea_Paul_Lab10/Models/ListProduct.cs
@@ -0,0 +1,14 @@
+﻿using SQLite;
+using SQLiteNetExtensions.Attributes;
+
+namespace Tirlea_Paul_Lab10.Models
+{
+    public class ListProduct
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+        [ForeignKey(typeof(ShopList))]
+        public int ShopListID { get; set; }
+        public int ProductID { get; set; }
+    }
+}
